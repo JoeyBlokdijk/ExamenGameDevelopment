8213b61dcb8636afdbf791ae7912c6580d9bfebe
Joey Blokdijk <574897@edu.rocmn.nl>
2025-06-18 09:10:08 +0200

    Base of the game

---
+++ K0788_BasisProgrammerenVanGames/BestEducationGame/index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Best Education Game</title>
+    <link rel='icon' type='image/png' href='logos/Logo1.png'>
+    <link rel='stylesheet' href='style.css'>
+    <script src='https://cdn.jsdelivr.net/npm/phaser@3/dist/phaser.js'></script>
+</head>
+
+<body>
+    <script src='main.js'></script>
+</body>
+
+</html>";
+
+?>
--- K0788_BasisProgrammerenVanGames/BestEducationGame/main.js
+++ K0788_BasisProgrammerenVanGames/BestEducationGame/main.js
@@ -1,1 +1,65 @@
-console.log("Gekoppeld");
+console.log("Connected");
+
+const config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+window.addEventListener("resize", () => {
+    game.scale.resize(window.innerWidth, window.innerHeight);
+});
+
+function preload() {
+    this.load.image("space", "assets/backgroundSpace.png");
+    this.load.image("rocket", "assets/LastRocket.png");
+}
+
+function create() {
+    this.bg = this.add.image(0, 0, "space").setOrigin(0, 0);
+    this.bg.setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
+
+    this.physics.world.setBounds(0, 0, this.sys.game.config.width, this.sys.game.config.height);
+
+    const centerX = this.sys.game.config.width / 2;
+
+    this.rocket = this.physics.add.sprite(centerX, 650, "rocket").setOrigin(0.5, 0.5);
+    this.rocket.setScale(0.08);
+
+    this.rocket.setCollideWorldBounds(true);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.keys = this.input.keyboard.addKeys("W,S,A,D");
+}
+
+function update() {
+    const speed = 600;
+
+    this.rocket.setVelocity(0);
+
+    if (this.cursors.left.isDown || this.keys.A.isDown) {
+        this.rocket.setVelocityX(-speed);
+    }
+    if (this.cursors.right.isDown || this.keys.D.isDown) {
+        this.rocket.setVelocityX(speed);
+    }
+    if (this.cursors.up.isDown || this.keys.W.isDown) {
+        this.rocket.setVelocityY(-speed);
+    }
+    if (this.cursors.down.isDown || this.keys.S.isDown) {
+        this.rocket.setVelocityY(speed);
+    }
+}
